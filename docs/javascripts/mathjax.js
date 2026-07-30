dc4e1d7197fa8664ae0715c6bef9eca721e2c294
acarcelenalgaba <acarcelenalgaba@educa.madrid.org>
2026-07-30 11:28:53 +0200

    1

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -4,12 +4,19 @@ window.MathJax = {
     displayMath: [["\\[", "\\]"], ["$$", "$$"]],
     processEscapes: true,
     processEnvironments: true,
-    packages: {'[+]': ['mhchem', 'color', 'colortbl']}, // añade mhchem y color
+    packages: {'[+]': ['mhchem', 'color', 'colortbl']}
   },
   options: {
     ignoreHtmlClass: "tex2jax_ignore|mathjax_ignore",
-    // añadimos 'arithmatex-box' para que lea también nuestra caja customizada
     processHtmlClass: "arithmatex|arithmatex-box"
   },
   loader: {load: ['[tex]/mhchem', '[tex]/color', '[tex]/colortbl']},
+  startup: {
+    pageReady: () => {
+      return MathJax.startup.defaultPageReady().then(() => {
+        // Marca en el DOM que MathJax ha terminado de pintar todas las ecuaciones
+        document.body.setAttribute("data-mathjax-ready", "true");
+      });
+    }
+  }
 };
